1626f837147456da2f3a8ba64489b9854b077908
Tommy Li <Tommy.yl.li@pccw.com>
2023-07-19 15:17:07 +0800

    fix: 🐛 update copyToClipboard function

--- js/copy_to_clipboard.js
+++ js/copy_to_clipboard.js
@@ -1,7 +1,10 @@
 var filter_url = "https://tommyim.github.io/adguard-filter-list/filter.txt";
 
-function copyToClipboard(url) {
-    var copyText = url;
-    navigator.clipboard.writeText(copyText);
-    alert("Copied the text: " + copyText);
+async function copyToClipboard(text) {
+    try {
+        await navigator.clipboard.writeText(text);
+        alert("Copied the text: " + text);
+    } catch (error) {
+        alert("Error copying text: ", error);
+    }
 }
