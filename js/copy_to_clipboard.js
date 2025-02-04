fab13711eeaf3c1ca0fc6076c2d18d27746052f6
Tommy Li <tommy.yl.li@pccw.com>
2025-02-04 10:46:35 +0800

    refactor: 💡 show absoluteUrl in alert

--- js/copy_to_clipboard.js
+++ js/copy_to_clipboard.js
@@ -5,7 +5,7 @@ async function copyToClipboard(text) {
     try {
         const absoluteUrl = new URL(text, window.location.href).href;
         await navigator.clipboard.writeText(absoluteUrl);;
-        alert(`Copied the text: ${text}`);
+        alert(`Copied the text: ${absoluteUrl}`);
     } catch (error) {
         console.error("Error copying text:", error);
         alert("Failed to copy text. Please try again.");
